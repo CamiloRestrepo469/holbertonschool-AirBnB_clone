fef3760e0e94857d39747bc0fa58844b6517131a
CamiloRestrepo469 <rsgermancamilo@gmail.com>
2023-07-11 13:25:26 -0500

    model base

--- models/base_model.py
+++ models/base_model.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 class BaseModel:
     """Base class for all models"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Create a constructor"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
