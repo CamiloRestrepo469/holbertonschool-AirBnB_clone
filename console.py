b009c65fabd085f308c2f4644cfbd9b2e10470e0
CamiloRestrepo469 <rsgermancamilo@gmail.com>
2023-07-13 08:40:29 -0500

    fix problem is OEF or quit

--- console.py
+++ console.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 import cmd
 
@@ -6,18 +6,20 @@ import cmd
 class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
 
-    def do_quit(self, arg):
+    def quit(self, arg):
         """Quit command to exit the program"""
         return True
 
-    def do_EOF(self, arg):
+    def EOF(self, arg):
         """Exit the program when End-of-File (EOF) is reached"""
         print()  # Print a newline before exiting
         return True
 
     def emptyline(self):
         """Do nothing when an empty line is entered"""
-        pass
+        if self.emptyline is None:
+            self.emptyline = '\n'
+        
 
 
 if __name__ == '__main__':
