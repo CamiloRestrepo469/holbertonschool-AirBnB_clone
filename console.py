23b7501301ad6ec2ebdf3e3f5fa92540f804c663
Briana1984 <brian.a.03@hotmail.com>
2023-07-14 10:07:45 -0500

    update

--- console.py
+++ console.py
@@ -14,6 +14,7 @@ from models import storage
 from typing import Tuple, Optional
 import inspect
 import shlex
+import re
 
 class_names_str = [
     "BaseModel", "User"
@@ -52,6 +53,15 @@ class HBNBCommand(cmd.Cmd):
             print(new_instance.id)
         else:
             print("** class doesn't exist **")
+            
+    def emptyline(self):
+        """any action"""
+        pass
+    
+    def main():
+        """Don't executed when imported"""
+        pass
+
 
     def do_show(self, line):
         tokens = line.split()
@@ -142,6 +152,36 @@ class HBNBCommand(cmd.Cmd):
                     flag = 1
             if flag == 0:
                 print("** no instance found **")
+                
+    def analyze(self, value):
+        """
+        Analyze the value and check if it
+        need to be converted to int or float
+        """
+        if value.isdigit():
+            return int(value)
+        if value.replace(".", "", 1).isdigit():
+            return float(value)
+        return value
+
+    def default(self, line):
+        """
+        If the command is not recognized, check
+        if the syntax is: <class name>.<method name> or not,
+        if the class name and the method name exists will be executed
+        """
+        if "." in line:
+            command = re.split(r"\.|\(|\)", line)
+
+            if command[0] in self.classes:
+                if command[1] == "show":
+                    self.do_show(f"{command[0]} {command[2][1:-1]}")
+                elif command[1] == "destroy":
+                    self.do_destroy(f"{command[0]} {command[2][1:-1]}")
+                elif command[1] == "count":
+                    print(len(self.get_instances(command[0])))
+                elif command[1] == "all":
+                    print(self.get_instances(command[0]))
 
 
 if __name__ == '__main__':
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
 import json
-import os
+from os import path
 from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
 from models.user import User
-
+from models.state import State
+from models.user import User
+from datetime import datetime
 
 class FileStorage:
     __file_path = "file.json"
     __objects = {}
+    classe = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
+              "Place": Place, "Review": Review, "State": State, "User": User}
 
     def all(self):
         return self.__objects
@@ -25,15 +33,11 @@ class FileStorage:
             json.dump(data, file)
 
     def reload(self):
+        """ deserializes the JSON file to __objects"""
         try:
-            with open(self.__file_path, "r") as file:
+            with open(self.__file-path, 'r') as file:
                 data = json.load(file)
-                for key, value in data.items():
-                    class_name, obj_id = key.split('.')
-                    class_obj = globals().get(class_name)
-                    if class_obj:
-                        instance = class_obj(**value)
-                        self.new(instance)
-        except FileNotFoundError:
+            for key in data: classmethod[data[key]["__class__"]](**data[key])
+        except Exception:
             pass
-
+       
